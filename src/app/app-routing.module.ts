cd1700e632e9a9f47a401b07a491eaacf762f73a
Tomek <tmszjaros@gmail.com>
2020-11-08 12:15:59 +0100

    BRLA

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,23 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {HomeComponent} from './component/home/home.component';
+import {AboutComponent} from './component/about/about.component';
+import {ContactComponent} from './component/contact/contact.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent
+  },
+  {
+    path: 'about',
+    component : AboutComponent
+  },
+  {
+    path: 'contact',
+    component : ContactComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
